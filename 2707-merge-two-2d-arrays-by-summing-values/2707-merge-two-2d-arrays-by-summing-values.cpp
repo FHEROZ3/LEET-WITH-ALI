96cf33f3d9092456a23469e40a7f8510c16c7add
FHEROZ3 <alieducation.363@gmail.com>
2025-03-02 19:36:51 +0530

    Time: 3 ms (32.71%), Space: 14.6 MB (82.78%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
@@ -9,7 +9,7 @@ public:
         {
             if(nums1[i][0]==nums2[j][0])
             {
-                A.push_back({nums1[i][0],nums1[i][1]+nums2[j][1]});
+                A.push_back({nums2[j][0],nums1[i][1]+nums2[j][1]});
                 i++;
                 j++;
             }
