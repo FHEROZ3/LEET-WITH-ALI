efff9f9c53aae1eb0ecfc3f7ea34ad1dc8347530
FHEROZ3 <alieducation.363@gmail.com>
2025-04-15 21:58:11 +0530

    Time: 405 ms (8.18%), Space: 171.9 MB (15.76%) - LeetHub

---
+++ 2280-count-good-triplets-in-an-array/2280-count-good-triplets-in-an-array.cpp
@@ -0,0 +1,23 @@
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+
+class Solution {
+public:
+    long long goodTriplets(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,int>A;
+        for(int i = 0;i < nums1.size();i++) 
+        A[nums1[i]] = i;
+        ordered_set st;
+        long long tot = 0,n = nums2.size();
+        for(int i = 0;i < n;i++)
+        {
+            int idx =A[nums2[i]];    
+            int left = st.order_of_key(idx); 
+            int right = (n - 1 - idx) - (st.size() - left); 
+            tot += (long long) left * right; 
+            st.insert(idx);
+        }
+        return tot;
+    }
+};
