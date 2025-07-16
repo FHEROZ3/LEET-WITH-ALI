02e3f51acf5d55e23def2fc73d998eb3fca4a191
FHEROZ3 <alieducation.363@gmail.com>
2025-07-16 12:23:45 +0530

    Time: 27 ms (18.24%), Space: 97 MB (26.18%) - LeetHub

---
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maximumLength(vector<int>& nums) {
+        int res = 0;
+        vector<vector<int>> A = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
+        for(auto& i : A)
+        {
+            int cnt = 0;
+            for(int num : nums)
+            {
+                if(num % 2 == i[cnt % 2])
+                {
+                    cnt++;
+                }
+            }
+            res = max(res, cnt);
+        }
+        return res;
+    }
+};
